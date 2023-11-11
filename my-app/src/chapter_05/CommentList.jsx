a327ea73d184053df4ab649c419efaa4cb5c2864
kkk <zwol10@naver.com>
2023-11-12 03:34:32 +0900

    feat: Book,Clock,Comment

---
+++ my-app/src/chapter_05/CommentList.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Comment from "./Comment";
+
+const comments = {
+    {
+        name: "이인제",
+        comment: "안녕하세요, 소플입니다."
+    },
+    {
+        name: "유재섣",
+        comment: "안녕하세요, 소플솔플."
+    },
+
+
+};
+
+function CommentList(props) {
+    return (
+        <div>
+            {comments.map((comment) => {
+                return (
+                    <Comment name={comment.name} comment={comment.comment} />
+                );
+            })}
+        </div>
+    );
+}
+
+export default CommentList;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,15 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// import Library from './chapter_03/Library';
+
+// import Clock from './chapter_04/Clock';
+import CommentList from './chapter_05/CommentList';
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <CommentList/>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
+// setInterval(() => {
+//   ReactDOM.render(
+//     <React.StrictMode>
+//       <Clock />
+//     </React.StrictMode>,
+//     document.getElementById('root')
+//   );
+// }, 1000);
+
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Library/>
+//   </React.StrictMode>,
+//   document.getElementById('root')
+
+// );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
