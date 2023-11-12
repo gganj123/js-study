727a3068148a13dc64f0025a171180cd9458af60
kkk <zwol10@naver.com>
2023-11-12 12:08:05 +0900

    error: book

--- my-app/src/chapter_05/Comment.jsx
+++ my-app/src/chapter_05/Comment.jsx
@@ -1,54 +1,54 @@
-import React from "react";
+// import React from "react";
 
-const style = {
-    wrapper: {
-        margin: 8,
-        padding: 8,
-        display: "fles",
-        flexDirection: "row",
-        border: "1px solid grey",
-        borderRadius: 16,
-    },
-    imageContainer: {},
-    image: {
-        width: 50,
-        height: 50,
-        borderRadius: 25,
-    },
-    contentContainer: {
-        marginLeft: 8,
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-    },
-    nameText: {
-        color: "black",
-        fontSize: 16,
-        fontWeight: "bold",
-    },
-    commentText: {
-        color: "black",
-        fontSize: 16,
-    },
-};
+// const style = {
+//     wrapper: {
+//         margin: 8,
+//         padding: 8,
+//         display: "fles",
+//         flexDirection: "row",
+//         border: "1px solid grey",
+//         borderRadius: 16,
+//     },
+//     imageContainer: {},
+//     image: {
+//         width: 50,
+//         height: 50,
+//         borderRadius: 25,
+//     },
+//     contentContainer: {
+//         marginLeft: 8,
+//         display: "flex",
+//         flexDirection: "column",
+//         justifyContent: "center",
+//     },
+//     nameText: {
+//         color: "black",
+//         fontSize: 16,
+//         fontWeight: "bold",
+//     },
+//     commentText: {
+//         color: "black",
+//         fontSize: 16,
+//     },
+// };
 
-function Comment(props) {
-    return (
-        <div style ={StyleSheet.wrapper}>
-            <div style={StyleSheet.imageContainer}>
-                <img
-                    src= "https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait+Placeholder.png"
-                    style={style.image}
-                />
-            </div>
-            <div style={StyleSheet.contentContainer}>
-                <span style={StyleSheet.nameText}>이인제</span>
-                <span style={StyleSheet.commentText}>
-                    제가 만든 첫 컴포넌트입니다.
-                </span>
-            </div>
-        </div>
-    );
-}
+// function Comment(props) {
+//     return (
+//         <div style ={styles.wrapper}>
+//             <div style={styles.imageContainer}>
+//                 <img
+//                     src= "https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png"
+//                     style={style.image}
+//                 />
+//             </div>
+//             <div style={styles.contentContainer}>
+//                 <span style={styles.nameText}>이인제</span>
+//                 <span style={styles.commentText}>
+//                     제가 만든 첫 컴포넌트입니다.
+//                 </span>
+//             </div>
+//         </div>
+//     );
+// }
 
-export default Comment;
+// export default Comment;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,17 +3,29 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-// import Library from './chapter_03/Library';
-
-// import Clock from './chapter_04/Clock';
-import CommentList from './chapter_05/CommentList';
+import Library from './chapter_03/Library';
 
 ReactDOM.render(
   <React.StrictMode>
-    <CommentList/>
+    <Library/>
   </React.StrictMode>,
   document.getElementById('root')
+
 );
+
+reportWebVitals();
+
+// import Clock from './chapter_04/Clock';
+// import CommentList from './chapter_05/CommentList';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <CommentList/>
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+
 // setInterval(() => {
 //   ReactDOM.render(
 //     <React.StrictMode>
@@ -24,17 +36,10 @@ ReactDOM.render(
 // }, 1000);
 
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <Library/>
-//   </React.StrictMode>,
-//   document.getElementById('root')
 
-// );
 
 
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
