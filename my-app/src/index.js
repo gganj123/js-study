7ccef1d6b7b6285232e3be5a9df8dde99aed1ab5
kkk <zwol10@naver.com>
2023-11-22 18:56:53 +0900

    feat: SignUp , Form

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -11,11 +11,13 @@ import NotificationList from './chapter_06/NotificationList';
 import ConfirmButton from './chapter_08/ConfirmButton';
 import LangdingPage from './chapter_09/LandingPage';
 import AttendanceBook from './chapter_10/AttendanceBook';
+import SignUp from './chapter_11/SignUp';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AttendanceBook/>
+    <SignUp/>
   </React.StrictMode>
 );
 
